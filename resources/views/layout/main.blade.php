96578f7327d3858aae66bffd7d985627d7942266
thestrange300 <tegarsangkeyboardist@gmail.com>
2023-03-30 15:15:08 +0700

    yeahh

---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,68 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="{{asset('img/Logo-Sekolah.png')}}" type="image/png">
+    {{-- <link href="{{ asset('/css/dropdown.css') }}" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.css" rel="stylesheet" /> --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+    <script src="https://fastly.jsdelivr.net/npm/echarts@5/dist/echarts.min.js"></script>
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    <link rel="stylesheet" type="text/css" href="https://unpkg.com/trix@2.0.0/dist/trix.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tw-elements/dist/css/index.min.css" />
+    <script src="https://cdn.tailwindcss.com/3.2.4"></script>
+    <script type="text/javascript" src="https://unpkg.com/trix@2.0.0/dist/trix.umd.min.js"></script>
+    <title>SDN Bubutan IV</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+  <script src="https://cdn.jsdelivr.net/npm/tw-elements/dist/js/index.min.js"></script>
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script>
+      AOS.init();
+    </script>
+    <div>
+        <aside class="ml-[-100%] fixed z-10 top-0 pb-3 px-6 w-full flex flex-col justify-between h-screen border-r bg-white transition duration-300 md:w-4/12 lg:ml-0 2xl:w-[13%]">
+            <div>
+                <div class="-mx-6 px-6 py-4">
+                    <a href="/test" title="home">
+                        <img src="{{asset('img/logo.png')}}" class="relative float-left" alt="logo sd">
+
+                    </a>
+                    <p class="pt-10 mt-10 text-center text-gray-500">Portal Dashboard</p>
+                </div>
+                
+                <ul class="space-y-2 tracking-wide mt-8">
+                    <li>
+                        <a href="/dashboard" aria-label="dashboard" class="relative px-4 py-3 flex items-center space-x-4 rounded-xl text-white bg-gradient-to-r from-sky-600 to-cyan-400">
+                            <svg class="-ml-1 h-6 w-6" viewBox="0 0 24 24" fill="none">
+                                <path d="M6 8a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2V8ZM6 15a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2v-1Z" class="fill-current text-cyan-400 dark:fill-slate-600"></path>
+                                <path d="M13 8a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2V8Z" class="fill-current text-cyan-200 group-hover:text-cyan-300"></path>
+                                <path d="M13 15a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2v-1Z" class="fill-current group-hover:text-sky-300"></path>
+                            </svg>
+                            <span class="-mr-1 text-sm font-semibold">CPL-BK</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        
+            <div class="px-6 -mx-6 pt-4 flex justify-between items-center border-t">
+                <button class="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+                    </svg>
+                    <span class="group-hover:text-gray-700">Logout</span>
+                </button>
+            </div>
+        </aside>
+        <div class="ml-auto mb-6 lg:w-[75%] xl:w-[80%] 2xl:w-[87%]">
+            <div class="sticky z-10 top-0 border-b bg-white lg:py-2.5">
+                <div class="px-6 flex items-center justify-between space-x-4 2xl:container">
+                    <h5 hidden class="text-2xl text-gray-800 font-bold lg:block">Dashboard</h5>                    
+                </div>
+            </div>
+        @yield('container')
+    </div>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,63 @@
+@extends ('layout.main')
+
+@section('container')
+
+<table class="table-auto w-full min-w-full divide-y divide-gray-300 text-center">
+    <thead class="bg-gray-50">
+      <tr>
+        <th scope="col" class="py-3.5 lg:px-4 text-left text-xs font-semibold text-gray-900">BK</th>
+        <th scope="col" class="py-3.5 lg:px-2 text-center text-xs font-semibold text-gray-900">Deskripsi BK</th>
+        <th scope="col" class="py-3.5 lg:px-2 text-center text-xs font-semibold text-gray-900">CPL-01</th>
+        <th scope="col" class="py-3.5 lg:px-2 text-center text-xs font-semibold text-gray-900">CPL-02</th>
+        <th scope="col" class="py-3.5 lg:px-2 text-center text-xs font-semibold text-gray-900">CPL-03</th>
+        <th scope="col" class="py-3.5 lg:px-2 text-center text-xs font-semibold text-gray-900">CPL-04</th>
+        <th scope="col" class="py-3.5 lg:px-2 text-center text-xs font-semibold text-gray-900">CPL-05</th>
+        <th scope="col" class="py-3.5 lg:px-2 text-center text-xs font-semibold text-gray-900">CPL-06</th>
+        <th scope="col" class="py-3.5 lg:px-2 text-center text-xs font-semibold text-gray-900">CPL-07</th>
+        <th scope="col" class="py-3.5 lg:px-2 text-center text-xs font-semibold text-gray-900">CPL-08</th>
+        <th scope="col" class="py-3.5 lg:px-2 text-center text-xs font-semibold text-gray-900">CPL-09</th>
+        <th scope="col" class="py-3.5 lg:px-2 text-center text-xs font-semibold text-gray-900">CPL-10</th>
+        <th scope="col" class="py-3.5 lg:px-2 text-center text-xs font-semibold text-gray-900">CPL-11</th>
+        <th scope="col" class="py-3.5 lg:px-2 text-center text-xs font-semibold text-gray-900">CPL-12</th>
+        <th scope="col" class="py-3.5 lg:px-2 text-center text-xs font-semibold text-gray-900">CPL-13</th>
+      </tr>
+    </thead>
+    <tbody class="divide-y divide-gray-200 bg-white">
+      <tr class="text-center place-content-center justify-center content-center place-self-center place-items-center">
+        <td class="py-3.5 lg:px-4 text-left text-xs font-semibold text-gray-900">BK01</td>
+        <td class="py-3.5 lg:px-2 text-left text-xs font-semibold text-gray-900">Foundation of Information Systems</td>
+        <td class="py-3.5 lg:px-2 text-left text-xs font-semibold text-gray-900 inline-flex items-center">
+                <label class="relative flex cursor-pointer items-center rounded-full p-3" for="checkbox" data-ripple-dark="true"> 
+                <input   type="checkbox"   class="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-gray-400 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-blue-700 checked:bg-blue-500 checked:before:bg-blue-500 hover:before:opacity-10"   id="checkbox"   checked /> <div class="pointer-events-none absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 text-white opacity-0 transition-opacity peer-checked:opacity-100">   <svg     xmlns="http://www.w3.org/2000/svg"     class="h-3.5 w-3.5"     viewBox="0 0 20 20"     fill="currentColor"     stroke="currentColor"     stroke-width="1"   >     <path       fill-rule="evenodd"       d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"       clip-rule="evenodd"     ></path>   </svg> </div>
+              </label>
+        </td>
+      </tr>
+      <tr class="text-center place-content-center justify-center content-center place-self-center place-items-center">
+        <td class="py-3.5 lg:px-4 text-left text-xs font-semibold text-gray-900">BK01</td>
+        <td class="py-3.5 lg:px-2 text-left text-xs font-semibold text-gray-900">Foundation of Information Systems</td>
+        <td class="py-3.5 lg:px-2 text-left text-xs font-semibold text-gray-900 inline-flex items-center">
+                <label class="relative flex cursor-pointer items-center rounded-full p-3" for="checkbox" data-ripple-dark="true"> 
+                <input   type="checkbox"   class="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-gray-400 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-blue-700 checked:bg-blue-500 checked:before:bg-blue-500 hover:before:opacity-10"   id="checkbox"   checked /> <div class="pointer-events-none absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 text-white opacity-0 transition-opacity peer-checked:opacity-100">   <svg     xmlns="http://www.w3.org/2000/svg"     class="h-3.5 w-3.5"     viewBox="0 0 20 20"     fill="currentColor"     stroke="currentColor"     stroke-width="1"   >     <path       fill-rule="evenodd"       d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"       clip-rule="evenodd"     ></path>   </svg> </div>
+              </label>
+        </td>
+      </tr>
+      <tr class="text-center place-content-center justify-center content-center place-self-center place-items-center">
+        <td class="py-3.5 lg:px-4 text-left text-xs font-semibold text-gray-900">BK01</td>
+        <td class="py-3.5 lg:px-2 text-left text-xs font-semibold text-gray-900">Foundation of Information Systems</td>
+        <td class="py-3.5 lg:px-2 text-left text-xs font-semibold text-gray-900 inline-flex items-center">
+                <label class="relative flex cursor-pointer items-center rounded-full p-3" for="checkbox" data-ripple-dark="true"> 
+                <input   type="checkbox"   class="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-gray-400 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-blue-700 checked:bg-blue-500 checked:before:bg-blue-500 hover:before:opacity-10"   id="checkbox"   checked /> <div class="pointer-events-none absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 text-white opacity-0 transition-opacity peer-checked:opacity-100">   <svg     xmlns="http://www.w3.org/2000/svg"     class="h-3.5 w-3.5"     viewBox="0 0 20 20"     fill="currentColor"     stroke="currentColor"     stroke-width="1"   >     <path       fill-rule="evenodd"       d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"       clip-rule="evenodd"     ></path>   </svg> </div>
+              </label>
+        </td>
+      </tr>
+      
+    </tbody>
+  </table>
+    <div class="pt-8 text-center place-content-center justify-center content-center place-self-center place-items-center">
+        <button class="middle none center rounded-lg bg-blue-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" data-ripple-light="true">
+            Simpan
+        </button>
+    </div>
+
+
+@endsection
